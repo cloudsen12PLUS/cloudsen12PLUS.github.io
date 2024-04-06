18dcc3264e49036ee34f14cb941f3199f5f1e2ca
pablo gutierrez <pabloyucragutierrez@gmail.com>
2024-04-05 19:56:16 -0500

    cambios en el header

--- src/partials/LanguageToggle.svelte
+++ src/partials/LanguageToggle.svelte
@@ -6,9 +6,9 @@
   <div class="relative z-10 dark:text-white mr-4 flex">
     <ul class="flex space-x-2 list-none">
       {#each Object.keys(translationsURL) as key}
-        <li class="uppercase font-semibold text-sm -mt-1 flex items-center">
+        <li class="uppercase font-semibold text-sm mt-0 flex items-center">
           <a href={translationsURL[key]}>
-            <img class="w-9 h-9 object-contain" id="img_language" src="" alt="" />
+            <img class="w-9 object-contain" id="img_language" src="" alt="" />
           </a>
         </li>
       {/each}
