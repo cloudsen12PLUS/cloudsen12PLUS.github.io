729952361fdcaaab7e0ff06a6795df6a2da38be1
pablo gutierrez <pabloyucragutierrez@gmail.com>
2024-04-08 22:24:01 -0500

    banderas agregadas

--- src/partials/LanguageToggle.svelte
+++ src/partials/LanguageToggle.svelte
@@ -7,7 +7,7 @@
     <ul class="flex space-x-2 list-none items-center">
       {#each Object.keys(translationsURL) as key}
         <li class="uppercase font-semibold -m-1 text-sm flex items-center">
-          <a href="{translationsURL[key]}" class="dark:text-white" id="img_language">
+          <a href="{translationsURL[key]}" class="dark:text-white" id="language_image">
           </a>
         </li>
       {/each}
